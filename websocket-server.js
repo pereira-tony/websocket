4a34e692428ace6b1d6ec7f6e25ec87ebac7a13a
Tony Pereira <tony@mbp-de-tony.home>
2020-05-04 14:41:19 +0200

    send message to server done

--- websocket-server.js
+++ websocket-server.js
@@ -1,17 +1,20 @@
+  
 const WebSocket = require('ws');
-
 
 const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 
-wss.on('connection', (WebSocket) => {
-    console.log('connected');
-
-    webSocket.on('close', () => {
-        console.log('disconnected');
-    });
-})
-
+wss.on('connection', webSocket => {
+    console.log('connection established');
+    webSocket.onmessage = (messageEvent) => {
+        const message = messageEvent.data;
+        console.log('message received ', message);
+    };
+});
+ 
 wss.on('error', (error) => {
     console.log('error occured ', error);
 });
 
+wss.on('close', function close() {
+    console.log('connection closed');
+});
