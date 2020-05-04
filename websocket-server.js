0ae93db890876053815239f0463840db2d2f9af8
Tony Pereira <tony@mbp-de-tony.home>
2020-05-04 16:29:06 +0200

    Envoi du message dans le chat

--- websocket-server.js
+++ websocket-server.js
@@ -5,16 +5,21 @@ const wss = new WebSocket.Server({ port: process.env.WS_PORT });
 
 wss.on('connection', webSocket => {
     console.log('connection established');
-    webSocket.onmessage = (messageEvent) => {
-        const message = messageEvent.data;
-        console.log('message received ', message);
-    };
+
+    webSocket.on('message', (message) => {
+        webSocket.send(message);
+    });
+
+    webSocket.send(JSON.stringify({
+        user: 'server',
+        message: 'Bonjour !'
+    }));
+
+    webSocket.on('close', () => {
+        console.log('connection closed by client');
+    });
 });
  
 wss.on('error', (error) => {
     console.log('error occured ', error);
-});
-
-wss.on('close', function close() {
-    console.log('connection closed');
 });
