0c2b925b14f1b1161a3953ae2603a9519bafa26a
Tony Pereira <tony@mbp-de-tony.home>
2020-05-04 12:16:00 +0200

    Initialisation du Websocket server et client + connection

--- websocket-server.js
+++ websocket-server.js
@@ -1,1 +1,8 @@
 const WebSocket = require('ws');
+
+
+const wss = new WebSocket.Server({ port: process.env.WS_PORT });
+
+wss.on('connection', (WebSocket) => {
+    console.log('client connected')
+})
